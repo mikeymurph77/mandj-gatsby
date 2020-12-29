6dcdfa6bfa99a0dbb6f068f1800d9d6c73ce8449
Michael Murphy <mmurphy@jobcase.com>
2020-12-28 23:01:39 -0500

    update registry page with working links

--- src/pages/registry.js
+++ src/pages/registry.js
@@ -7,7 +7,6 @@ import Header from "../components/Header/Header"
 import Footer from "../components/Footer/Footer"
 import SectionTitle from "../components/Helpers/SectionTitle"
 import RegistryItem from "../components/RegistryItem/RegistryItem"
-import CenteredDiv from "../components/Helpers/CenteredDiv"
 
 const RegistryRow = styled.div`
   display: flex;
@@ -16,40 +15,23 @@ const RegistryRow = styled.div`
   margin: ${rhythm(1)} ${rhythm(1)} ${rhythm(5)};
 `
 
-const CommingSoon = styled.img`
-  max-width: 200px;
-
-  @media screen and (min-width: 601px) {
-    max-width: 400px;
-  }
-`;
-
 export default function Index() {
   return (
     <Layout>
       <Header />
       <SectionTitle title="Registry" />
-
-      <CenteredDiv>
-        <CommingSoon src="https://res.cloudinary.com/drzgzthur/image/upload/v1600276165/MandJ/Coming-Soon-image.jpg" alt="Coming Soon" />
-      </CenteredDiv>
-      {/* <RegistryRow>
+      <RegistryRow>
         <RegistryItem
           store="Crate & Barrel"
-          link="https://www.crateandbarrel.com/gift-registry/"
+          link="https://www.crateandbarrel.com/gift-registry/jillian-marlowe-and-michael-murphy/r6166197"
           logoUrl="https://i.pinimg.com/originals/67/3d/8d/673d8d1ae65e5b9cdbd27bd76e6c6595.jpg"
         />
         <RegistryItem
           store="Target"
-          link="https://www.target.com/gift-registry/"
+          link="https://www.target.com/gift-registry/giftgiver?registryId=b13a55fe702346bdb5fa92ecca61d4ec"
           logoUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Target_Corporation_logo_%28vector%29.svg/1024px-Target_Corporation_logo_%28vector%29.svg.png"
         />
-        <RegistryItem
-          store="Home Depot"
-          link="https://www.homedepot.com/collection/home-decor/perfect-registry-picks"
-          logoUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/TheHomeDepot.svg/227px-TheHomeDepot.svg.png"
-        />
-      </RegistryRow> */}
+      </RegistryRow>
       <Footer />
     </Layout>
   )
